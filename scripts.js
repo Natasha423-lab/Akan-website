5180d97a43b185e4179354056462db5e7d107eea
Natasha <natashaongera@gmail.com>
2025-01-24 15:21:52 +0300

    Update HTML

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 const maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 const femaleNames=["AKosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-function calculateAkanName(event){eventPreventDefault();}
+function calculateAkanName(event){event.preventDefault();}
+//Get form inputs
+const MM = parseInt(document.getElementById("month").value);
+const YY = parseInt(document.getElementById("year").value);
+const DD = parseInt(document.getElementById("day").value);
+const gender=document.querySelector("gender")
+const CC=Math.floor(year/100) 
+const day=Math.floor( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7
+console.log=(MM)
+console.log=(YY)
+console.log=(DD)
+console.log=(gender)
+const Index = ((day %7)+7)%7;
